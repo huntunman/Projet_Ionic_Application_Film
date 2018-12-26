566aad310b4574bbedcdf316daeb3a5ecfd2e1bb
huntunman <43952602+huntunman@users.noreply.github.com>
2018-12-26 05:10:25 +0100

    2.5.2
    la même plus avancer

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { MovieApiProvider } from '../../providers/movie-api/movie-api';
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
   qrData = null;
   createdCode = null;
   scannedCode = null;
 
-  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
-    
+  movies: any;
+  movie_list: any;
+
+  constructor(public navCtrl: NavController, 
+    private barcodeScanner: BarcodeScanner,    
+    private movieApiProvider: MovieApiProvider
+    ) {
+  }
+
+  ionViewDidLoad() {
+    this.movieApiProvider.getMovies().subscribe(data => {
+      this.movies = data;
+      this.movies = this.movies.results;
+      this.movie_list = data;
+      this.movie_list = this.movie_list.results;
+    })
+    console.log('ionViewDidLoad MovieListPage');
   }
 
   createCode(){
@@ -26,6 +42,25 @@ export class AboutPage {
     }, (err) => {
         console.log('Error: ', err);
     });
+  }
+
+  initializeMovies() {
+    this.movies = this.movie_list;
+  }
+
+  getMovie(ev: any) {
+    // Reset items back to all of the items
+    this.initializeMovies();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.movies = this.movies.filter((movie) => {
+        return (movie.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
